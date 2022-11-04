517d1f1d48cc9ba191175cd47df959a9f1f73b57
Aracely Alba @AshkoreDev <aracelyalba8@gmail.com>
2022-11-04 21:26:11 +0100

    Added product list HTML structure

---
+++ src/js/product-list.js
@@ -0,0 +1,85 @@
+
+const productList = [];
+const productListContainer = document.getElementById('productListContainer');
+const productCardFragment = document.createDocumentFragment();
+
+productList.push([
+  {
+	name: 'Bicicleta',
+	price: 500,
+	image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+	},
+	{
+	name: 'Pantalla',
+	price: 200,
+	image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+	},
+	{
+	name: 'Computador',
+	price: 600,
+	image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+	},
+	{
+	name: 'Cama',
+	price: 400,
+	image: 'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'
+	}
+]);
+
+for(product of productList) {
+
+	const productCard = document.createElement('div');
+	productCard.clasList.add('product__card');
+
+	const productCardImg = document.createElement('img');
+	productCardImg.setAttribute('src', product.image);
+	productCardImg.setAttribute('alt', `#{product.name} foto`);
+
+	const productInfo = document.createElement('div');
+	productInfo.clasList.add('product__info');
+
+	const productInfoDiv = document.createElement('div');
+
+	const productInfoPrice = document.createElement('p');
+	productInfoPrice.innerText = `$#{product.price}`;
+
+	const productInfoName = document.createElement('p');
+	productInfoName.innerText = `$#{product.name}`;
+
+	const productFigure = document.createElement('figure');
+
+	const productFigureImg = document.createElement('img');
+	productFigureImg.setAttribute('src', './../images/bt_add_to_cart.svg');
+	productFigureImg.setAttribute('alt', `#{product.name} foto`);
+
+	productFigure.appendChild(productFigureImg);
+	productInfoDiv.appendChild(productInfoPrice);
+	productInfoDiv.appendChild(productInfoName);
+	productInfo.appendChild(productInfoDiv);
+
+	productCard.appendChild(productCardImg);
+	productCard.appendChild(productInfo);
+	productCard.appendChild(productFigure);
+
+	productCard.append(productCard);
+
+}
+
+productListContainer.append(productCardFragment);
+
+console.log(productList);
+
+// <div class="product-card">
+//         <img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
+
+//         <div class="product-info">
+//           <div>
+//             <p>$120,00</p>
+//             <p>Bike</p>
+//           </div>
+
+//           <figure>
+//             <img src="./icons/bt_add_to_cart.svg" alt="">
+//           </figure>
+//         </div>
+//       </div>
